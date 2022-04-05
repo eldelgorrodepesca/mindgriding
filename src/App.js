1c01b8797500799f8baf3189426f1e0e74025a5f
eldelgorrodepesca <6001754@viewnext.com>
2022-04-05 09:23:27 +0200

    primer commit con la estructura basica del proyecto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="title">MIND GRIDING</h1>
+        <nav class="navigation">
+          <ul>
+            <li>
+              <a href="index.html">Inicio</a>
+            </li>
+            <li>
+              <a href="asignaturas.html">Asignaturas</a>
+            </li>
+            <li>
+              <a href="progreso.html">Progreso</a>
+            </li>
+            <li>
+              <a href="calendario.html">Calendario</a>
+            </li>
+            <li>
+              <a href="notas.html">Notas</a>
+            </li>
+            <li>
+              <a href="login.html">Iniciar Sesión</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
